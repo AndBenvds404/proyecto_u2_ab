d2af41813fc384d27c655ee5afebbf9518601715
AndBenvds404 <andres_benavides17@hotmail.com>
2022-06-24 20:06:18 -0600

    Taller 14

--- src/main/java/com/uce/edu/demo/ProyectoU2AbApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AbApplication.java
@@ -1,13 +1,21 @@
 package com.uce.edu.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoU2AbApplication {
+public class ProyectoU2AbApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		
 	}
 
 }
