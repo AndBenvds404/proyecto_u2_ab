0bc827b6bc25d176f4d23f9b67f744c1d6db2943
AndBenvds404 <andres_benavides17@hotmail.com>
2022-07-22 20:13:01 -0600

    TALLER 25 JPA mapeo OneToMany

--- src/main/java/com/uce/edu/demo/ProyectoU2AbApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AbApplication.java
@@ -1,7 +1,5 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.Ciudadano;
-import com.uce.edu.demo.repository.modelo.Pasaporte;
+import com.uce.edu.demo.repository.modelo.onetomany.Habitacion;
+import com.uce.edu.demo.repository.modelo.onetomany.Hotel;
 import com.uce.edu.demo.service.ICiudadanoJpaService;
+import com.uce.edu.demo.service.IHabitacionService;
+import com.uce.edu.demo.service.IHotelService;
 
 
 @SpringBootApplication
@@ -19,6 +19,11 @@ public class ProyectoU2AbApplication implements CommandLineRunner{
 	
 	Logger LOG = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 	
+	@Autowired
+	private IHotelService ihotelService;
+	
+	@Autowired
+	private IHabitacionService iHabitacionService;
 	
 	@Autowired
 	private ICiudadanoJpaService iCiudadanoJpaService;
@@ -32,41 +37,22 @@ public class ProyectoU2AbApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Ciudadano ciu1 = new Ciudadano();
-		ciu1.setNombre("Mike6");
-		ciu1.setApellido("Wasouski6");
-		ciu1.setCedula("172706");
-		ciu1.setFechaNacimiento(LocalDateTime.of(1996, 6, 25, 17, 41));
+		Hotel hotel1 = new Hotel();
+		hotel1.setId(1);
 		
+		Habitacion habitacion1 = new Habitacion();
+		habitacion1.setNumero("A1");
+		habitacion1.setPiso("2");
+		habitacion1.setTipo("matrimonial");
+		habitacion1.setHotelr(hotel1);
 		
-		Pasaporte pasa1 = new Pasaporte();
-		pasa1.setNumero("006");
-		pasa1.setFechaEmision(LocalDateTime.of(2012,6,8, 12, 21));
-		pasa1.setFechaCaducidad(LocalDateTime.now());
-		pasa1.setCiudadano(ciu1);
-		ciu1.setPasaporte(pasa1);
+		Habitacion habitacion2 = new Habitacion();
+		habitacion2.setNumero("A2");
+		habitacion2.setTipo("solitario");
+		habitacion2.setPiso("2");
+		habitacion2.setHotelr(hotel1);
 		
-		//Insertar 
-		this.iCiudadanoJpaService.insertar(ciu1);
-		
-		//Actualizar
-
-		ciu1.setNombre("Miguel");
-		ciu1.setApellido("Walter");
-		ciu1.setCedula("172705");
-		ciu1.setFechaNacimiento(LocalDateTime.of(1996, 6, 25, 17, 41));
-		pasa1.setFechaCaducidad(LocalDateTime.of(2021,2,2, 12, 22));
-		ciu1.setPasaporte(pasa1);
-		this.iCiudadanoJpaService.actualizar(ciu1);
-		
-		//Buscar
-		LOG.info("Ciudadano: "+this.iCiudadanoJpaService.buscarPorCedula("172701"));
-		
-		//Eliminar
-		this.iCiudadanoJpaService.eliminarPorCedula("172704");
-		
-		
-		
+		this.iHabitacionService.insertar(habitacion1);
 
 		
 	}
